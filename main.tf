e61de7c9ccc7d4b456221a254f7198b89e39eab7
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-28 07:52:35 -0400

    added changes

--- main.tf
+++ main.tf
@@ -16,18 +16,30 @@ resource "aws_security_group" "allow_tls" {
 variable "ingress_rules" {
   description = "A map of ingress rules"
   type = map(object({
-    cidr_ipv4   = list
+    cidr_ipv4   = list(string)
     from_port   = number
     ip_protocol = string
     to_port     = number
+    desciption  = string
   }))
 }
-resource "aws_vpc_security_group_ingress_rule" "example" {
-  for_each          = var.ingress_rules
-  security_group_id = aws_security_group.allow_tls.id
+dynamic "ingress" {
+  for_each = var.ingress_rules
+  content {
+    from_port   = ingress.value.from_port
+    to_port     = ingress.value.to_port
+    protocol    = ingress.value.ip_protocol
+    cidr_blocks = ingress.value.cidr_ipv4
 
-  cidr_ipv4   = each.value.cidr_ipv4
-  from_port   = each.value.from_port
-  ip_protocol = each.value.ip_protocol
-  to_port     = each.value.to_port
+    description = ingress.value.desciption
+  }
 }
+#resource "aws_vpc_security_group_ingress_rule" "example" {
+#  for_each          = var.ingress_rules
+#  security_group_id = aws_security_group.allow_tls.id
+#
+#  cidr_ipv4   = each.value.cidr_ipv4
+#  from_port   = each.value.from_port
+#  ip_protocol = each.value.ip_protocol
+#  to_port     = each.value.to_port
+#
