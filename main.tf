08d830440552f27d6001ada28e21c8a1a4530f0a
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-28 07:42:03 -0400

    added  main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,23 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
+
+variable "ingress_rules" {
+  description = "A map of ingress rules"
+  type = map(object({
+    cidr_ipv4   = list
+    from_port   = number
+    ip_protocol = string
+    to_port     = number
+  }))
+}
+resource "aws_vpc_security_group_ingress_rule" "example" {
+  for_each          = var.ingress_rules
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = each.value.cidr_ipv4
+  from_port   = each.value.from_port
+  ip_protocol = each.value.ip_protocol
+  to_port     = each.value.to_port
+}
